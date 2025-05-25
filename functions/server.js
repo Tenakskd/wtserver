9241b4143937bcd6280803b781db6506e969b5e7
Tenakskd <anzuday7777@gmail.com>
2025-05-25 11:54:13 +0900

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,4 @@
-"const express = require("express");
+const express = require("express");
 const serverless = require("serverless-http");
 const axios = require('axios');
 const bodyParser = require('body-parser');
